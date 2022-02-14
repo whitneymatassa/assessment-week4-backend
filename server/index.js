1327faaa5f2d7adcb628ef57d49e15db94b6cb5a
Whitney Matassa <whitney.matassa@gmail.com>
2022-02-14 08:54:05 -0500

    3 Features

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const res = require("express/lib/response");
+const { send } = require("express/lib/response");
 
 const app = express();
 
@@ -17,9 +19,64 @@ app.get("/api/compliment", (req, res) => {
   // choose random compliment
   let randomIndex = Math.floor(Math.random() * compliments.length);
   let randomCompliment = compliments[randomIndex];
-
   res.status(200).send(randomCompliment);
   
 });
 
-app.listen(4000, () => console.log("Server running on 4000"));
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["You're going to win an award", 
+            "You are going to be a huge success", 
+            "You will travel the world", 
+            "You are going to be very rich", 
+            "You will write a book",
+  ];
+
+  let fortuneIndex = Math.floor(Math.random()*fortunes.length);
+  let randomFortune = fortunes[fortuneIndex];
+  res.status(200).send(randomFortune);
+
+});
+
+app.get("/api/quote", (req, res) => {
+  const quotes = ['"You do not find the happy life, you make it!" - Camilla Kimball', 
+            '"The most wasted of days is one without laughter." - EE Cumings', 
+            '"Make each day a masterpiece." - John Wooden', 
+  ];
+
+  let quoteIndex = Math.floor(Math.random()*quotes.length);
+  let randomQuote = quotes[quoteIndex];
+  res.status(200).send(randomQuote);
+
+});
+
+
+const newName = []
+const id = 1
+
+app.get(`/api/name`, (req, res) => {
+  res.status(200).send(newName)
+});
+
+app.post('/api/name', (req, res) => {
+  const { name } = req.body;
+  let addName = {
+    id: id,
+    name: name
+  };
+  items.push(addName)
+  id++
+
+  res.status(200).send(newName);
+});
+
+app.delete(`/api/name/:id`, (req, res) => {
+  let index = names.findIndex(elem => elem.id === + req.params.id)
+  names.splice(index,1)
+  res.status(200).send('name deleted')
+});
+
+
+app.get(`api/vacations`, (req, res) => {
+  const message = "Thanks for submitting your preference!"
+  res.status(200).send(message);
+});
